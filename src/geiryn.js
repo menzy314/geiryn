a3ee6a4497cf0eeb0c52a33261e615414aec6044
Menzy <menna@troi.org>
2022-09-14 11:12:35 +0800

    Implement geiryn.Model
    TODO: write unit tests

--- src/geiryn.js
+++ src/geiryn.js
@@ -57,3 +57,40 @@ geiryn.evaluate = function ( word, guess ) {
 	}
 	return scores;
 };
+
+/**
+ * Game logic
+ *
+ * @constructor
+ * @param {string} answer The answer
+ */
+geiryn.Model = function ( answer ) {
+	this.guesses = [];
+	this.letterStates = {};
+	this.answer = answer;
+};
+
+/**
+ * Process user's guess.
+ *
+ *
+ * @method
+ * @param {string} text The guess
+ * @return {boolean} Is text a word
+ */
+geiryn.Model.prototype.guess = function ( text ) {
+	if ( geiryn.isWord( text ) === false ) {
+		return false;
+	}
+	this.guesses.push( text );
+	var scores = geiryn.evaluate( this.answer, text );
+	for ( var i = 0; i < 5; i++ ) {
+		var letter = text[ i ];
+		var score = scores[ i ];
+		var pastScore = this.letterStates[ letter ];
+		if ( pastScore === undefined || score > pastScore ) {
+			this.letterStates[ letter ] = score;
+		}
+	}
+	return true;
+};
