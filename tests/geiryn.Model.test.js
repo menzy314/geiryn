dbae9761c287378d0d1486c51b8fc1df0964e236
Menzy <menna@troi.org>
2022-09-15 11:26:45 +0800

    Add unit tests for geiryn.Model#guess

--- tests/geiryn.Model.test.js
+++ tests/geiryn.Model.test.js
@@ -14,3 +14,21 @@ QUnit.test( 'constructor', function ( assert ) {
 	assert.deepEqual( m.letterStates, {}, 'no letter states yet' );
 	assert.deepEqual( m.answer, 'leery', 'the answer is stored correctly' );
 } );
+
+QUnit.test( 'guess', function ( assert ) {
+	var m = new geiryn.Model( 'leery' );
+	var result1 = m.guess( 'iiili' );
+	assert.deepEqual( result1, false, 'non-word is rejected' );
+	var result2 = m.guess( 'bread' );
+	assert.deepEqual( result2, true, 'word is accepted' );
+	assert.deepEqual( m.guesses, [ 'bread' ], 'only the real word is stored as a guess' );
+	assert.deepEqual( m.letterStates, { b: 0, r: 1, e: 2, a: 0, d: 0 }, 'letter states are updated' );
+	var result3 = m.guess( 'levee' );
+	assert.deepEqual( result3, true, 'word is accepted' );
+	assert.deepEqual( m.guesses, [ 'bread', 'levee' ], 'only the real words are stored as a guess' );
+	assert.deepEqual( m.letterStates, { b: 0, r: 1, e: 2, a: 0, d: 0, l: 2, v: 0 }, 'letter states are updated' );
+	var result4 = m.guess( 'grape' );
+	assert.deepEqual( result4, true, 'word is accepted' );
+	assert.deepEqual( m.guesses, [ 'bread', 'levee', 'grape' ], 'only the real words are stored as a guess' );
+	assert.deepEqual( m.letterStates, { b: 0, r: 1, e: 2, a: 0, d: 0, l: 2, v: 0, g: 0, p: 0 }, 'letter states are updated' );
+} );
