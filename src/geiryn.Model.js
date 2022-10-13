menzy314/geiryn

commit 1e2bf38e785debb95018431efc55a15c80b5aed0

Support digraph input on hardware keyboard

src/geiryn.Model.js
@@ -15,6 +15,7 @@ geiryn.Model = function ( answer ) {
 	this.keyStates = {};
 	this.answer = answer;
 	this.nextGuess = [];
+	this.maybeDigraph = false;
 };
 
 /**
@@ -45,17 +46,31 @@ geiryn.Model.prototype.guess = function ( text ) {
 };
 
 geiryn.Model.prototype.pushLetter = function ( letter ) {
+	if ( this.maybeDigraph === true ) {
+		this.maybeDigraph = false;
+		var digraph = this.nextGuess[ this.nextGuess.length - 1 ] + letter;
+		if ( digraph.match( /^(ch|dd|ff|ng|ll|rh|th)$/ ) ) {
+			this.nextGuess.pop();
+			this.nextGuess.push( digraph );
+			return;
+		}
+	}
 	if ( this.nextGuess.length < 5 ) {
 		this.nextGuess.push( letter );
+		if ( letter.match( /^[cdfnlrt]$/ ) ) {
+			this.maybeDigraph = true;
+		}
 	}
 };
 geiryn.Model.prototype.popLetter = function () {
 	this.nextGuess.pop();
+	this.maybeDigraph = false;
 };
 geiryn.Model.prototype.submitGuess = function () {
 	var currentGuess = this.nextGuess.slice();
 	this.guess( currentGuess );
 	this.nextGuess.splice( 0 );
+	this.maybeDigraph = false;
 };
 
 geiryn.Model.prototype.getScoreEmojis = function () {

src/geiryn.View.css
@@ -20,6 +20,7 @@ body {
 }
 .geiryn-board-letter-next {
 	border: solid black 3px;
+	text-decoration: underline;
 }
 .geiryn-board-letter-0 {
 	background-color: darkgray;
@@ -34,6 +35,7 @@ body {
 	color: white;
 }
 .geiryn-keyboard {
+	text-transform: uppercase;
 }
 .geiryn-keyboard-row {
 	margin: 0.3em;

src/geiryn.View.js
@@ -24,6 +24,9 @@ geiryn.View = function ( model ) {
 };
 
 geiryn.View.prototype.onKeyDown = function ( ev ) {
+	if ( !ev.shiftKey && !ev.ctrlKey && !ev.metaKey ) {
+		ev.preventDefault();
+	}
 	if ( ev.key.match( /^[A-Za-z]$/ ) ) {
 		this.model.pushLetter( ev.key );
 		this.draw();
@@ -34,6 +37,10 @@ geiryn.View.prototype.onKeyDown = function ( ev ) {
 	} else if ( ev.keyCode === 13 ) {
 		// Enter
 		this.model.submitGuess();
+		this.draw();
+	} else if ( ev.keyCode === 32 ) {
+		// Space
+		this.model.maybeDigraph = false;
 		this.draw();
 	}
 };
@@ -115,7 +122,12 @@ geiryn.View.prototype.createNextGuessRow = function ( board, letters ) {
 	board.appendChild( nextGuessRow );
 	for ( var i = 0; i < 5; i++ ) {
 		var letter = letters[ i ];
-		var isNext = ( i === letters.length );
+		var isNext;
+		if ( !this.model.maybeDigraph ) {
+			isNext = ( i === letters.length );
+		} else {
+			isNext = ( i === letters.length - 1 );
+		}
 		this.createBoardLetter( nextGuessRow, letter, undefined, isNext );
 	}
 };

commit 9908531fc279fde903ad951e759b05eb45c88977

Fix software keyboard regression from 1e2bf38e
The rule text-transform: uppercase was added to .geiryn-keyboard which
caused uppercase letters to be input into the guesses array, and fail to
match the lowercase words in the wordlist.

Now model accepts letters in any case and uses toLowerCase.

src/geiryn.Model.js
@@ -46,9 +46,10 @@ geiryn.Model.prototype.guess = function ( text ) {
 };
 
 geiryn.Model.prototype.pushLetter = function ( letter ) {
+	var lowerLetter = letter.toLowerCase();
 	if ( this.maybeDigraph === true ) {
 		this.maybeDigraph = false;
-		var digraph = this.nextGuess[ this.nextGuess.length - 1 ] + letter;
+		var digraph = this.nextGuess[ this.nextGuess.length - 1 ] + lowerLetter;
 		if ( digraph.match( /^(ch|dd|ff|ng|ll|rh|th)$/ ) ) {
 			this.nextGuess.pop();
 			this.nextGuess.push( digraph );
@@ -56,8 +57,8 @@ geiryn.Model.prototype.pushLetter = function ( letter ) {
 		}
 	}
 	if ( this.nextGuess.length < 5 ) {
-		this.nextGuess.push( letter );
-		if ( letter.match( /^[cdfnlrt]$/ ) ) {
+		this.nextGuess.push( lowerLetter );
+		if ( lowerLetter.match( /^[cdfnlrt]$/ ) ) {
 			this.maybeDigraph = true;
 		}
 	}